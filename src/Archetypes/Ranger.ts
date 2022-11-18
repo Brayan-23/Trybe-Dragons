8353136e93d8c1d43fd6a26e3352c3a3c63533bd
Brayan Santos <brayan.santosrosa@gmail.com>
2022-11-18 17:19:47 -0300

    Requisito 05 concluido

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,20 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetypes';
+
+export default class Ranger extends Archetype {
+  private static _count = 0;
+  private _power: EnergyType;
+  constructor(n: string, power: EnergyType = 'stamina') {
+    super(n);
+    this._power = power;
+  }
+
+  static createdArchetypeInstances(): number {
+    this._count += 1;
+    return this._count;
+  }
+
+  get energyType(): EnergyType {
+    return this._power;
+  }
+}
